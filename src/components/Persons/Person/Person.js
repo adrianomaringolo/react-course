6fe20167eab58d067a6d41bbd718716511d1c303
Adriano Maringolo Gomes <adrianomaringolo@gmail.com>
2018-07-09 22:57:21 -0300

    Component lifecycle

---
+++ src/components/Persons/Person/Person.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import classes from './Person.css';
+
+class Person extends Component {
+
+  constructor(props) {
+    super(props);
+    console.log('[Person.js] Inside constructor', props);
+  }
+
+  componentWillMount() {
+    console.log('[Person.js] Inside componentWillMount');
+  }
+
+  componentDidMount() {
+    console.log('[Person.js] Inside componentDidMount');
+  }
+
+
+  render() {
+    console.log('[Person.js] Inside render');
+    return (
+      <div className={classes.Person}>
+        <p onClick={ this.props.click }>My name is { this.props.name } and my age is { this.props.age } !</p>
+        <p>{ this.props.children }</p>
+        <input type="text" onChange={ this.props.changed } value={ this.props.name }/>
+      </div>
+    )
+  }
+}
+
+export default Person;
